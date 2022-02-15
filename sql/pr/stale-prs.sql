5b7145e8796d522ce8ab3f924bcc017a5f19bb55
Michał Ślizak <michal.slizak+github@gmail.com>
2022-02-15 16:43:16 +0100

    Add top 20 stale PRs

---
+++ sql/pr/stale-prs.sql
@@ -0,0 +1,11 @@
+-- Stale PRs
+SELECT
+  title
+, updated_at
+, current_timestamp - updated_at AS untouched_for
+FROM
+  pulls
+WHERE owner = 'trinodb' AND repo = 'trino' AND state = 'open' AND NOT draft
+ORDER BY untouched_for DESC
+LIMIT 20
+;
