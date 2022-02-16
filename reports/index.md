7b07d64e3bf6aa188378cd206f0465c6531ac556
Michał Ślizak <michal.slizak+github@gmail.com>
2022-02-16 10:26:17 +0100

    Add time_since_review and time_since_push to top 20 stale PRs

---
+++ reports/index.md
@@ -0,0 +1,10 @@
+Trino Project Reports
+=====================
+
+#  CI/CD pipeline reports
+
+[page](https://nineinchnick.github.io/trino-cicd/reports/ci-cd/)
+
+#  Pull request pipeline reports
+
+[page](https://nineinchnick.github.io/trino-cicd/reports/pr/)
--- sql/pr/stale-prs.sql
+++ sql/pr/stale-prs.sql
@@ -1,12 +1,19 @@
 -- Stale PRs
 SELECT
-  number
-, title
-, updated_at
-, current_timestamp - updated_at AS untouched_for
+  pulls.number
+, pulls.title
+, pulls.updated_at
+, current_timestamp - pulls.updated_at AS untouched_for
+, current_timestamp - max(reviews.submitted_at) AS time_since_review
+, current_timestamp - max(pull_commits.committer_date) AS time_since_push
 FROM
   pulls
-WHERE owner = 'trinodb' AND repo = 'trino' AND state = 'open' AND NOT draft
+LEFT JOIN
+  reviews ON pulls.number = reviews.pull_number
+LEFT JOIN
+  pull_commits ON pulls.number = pull_commits.pull_number
+WHERE pulls.owner = 'trinodb' AND pulls.repo = 'trino' AND pulls.state = 'open' AND NOT pulls.draft
+GROUP BY 1, 2, 3, 4
 ORDER BY untouched_for DESC
 LIMIT 20
 ;
