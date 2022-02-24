0121e74d072dc5a80c91746c8209d9137e4c05c9
Jan Was <jan.was@starburstdata.com>
2022-02-24 13:09:02 +0100

    Fix max concurrent jobs query

--- sql/ci-cd/concurrent-jobs-per-day.sql
+++ sql/ci-cd/concurrent-jobs-per-day.sql
@@ -1,36 +1,32 @@
 -- Max concurrent jobs per day
-WITH overlapping_jobs AS (
+WITH minutes AS (
+  SELECT date_add('minute', -minutes.minute, date_add('day', -days_ago.nr, current_timestamp)) AS minute
+  FROM unnest(sequence(0, 13)) AS days_ago(nr)
+  CROSS JOIN unnest(sequence(0, 24 * 60 - 1)) AS minutes(minute)
+),
+overlapping_jobs AS (
   SELECT
-    cast(r1.created_at AS date) AS day
-    , r1.id AS run_id
-    , j1.id AS job_id
-    , count(j2.id) AS concurrent_jobs_count
-  FROM runs r1
-  LEFT JOIN runs r2 ON (r1.owner, r1.repo, r1.name, cast(r1.created_at as date)) = (r2.owner, r2.repo, r2.name, cast(r2.created_at as date))
-  JOIN jobs j1 ON j1.run_id = r1.id
-  LEFT JOIN jobs j2 ON j2.run_id = r2.id AND j2.id != j1.id AND j2.completed_at > j1.started_at AND j2.started_at <= j1.completed_at
+    minutes.minute
+    , count(j.id) AS concurrent_jobs_count
+    , count(distinct r.id) AS concurrent_runs_count
+  FROM minutes
   -- exclude jobs that ran for 0 seconds, most likely immediately cancelled, but we don't want to exclude all cancelled jobs
-  AND j2.started_at != j2.completed_at
-  WHERE r1.owner = 'trinodb' AND r1.repo = 'trino' AND r1.name = 'ci'
-  AND r1.created_at > CURRENT_DATE - INTERVAL '14' DAY
-  GROUP BY 1, 2, 3
+  JOIN jobs j ON minutes.minute BETWEEN j.started_at AND j.completed_at AND j.started_at != j.completed_at
+  JOIN runs r ON r.id = j.run_id AND r.owner = 'trinodb' AND r.repo = 'trino' AND r.name = 'ci'
+  GROUP BY 1
 ),
 main AS (
   SELECT
-    day
-    , count(distinct run_id) AS runs_count
-    , count(*) AS jobs_count
+    cast(minute AS date) AS day
     , max(concurrent_jobs_count) AS max_concurrent_jobs_count
-    , max_by(job_id, concurrent_jobs_count) AS max_concurrent_job
+    , max(concurrent_runs_count) AS max_concurrent_runs_count
   FROM overlapping_jobs
   GROUP BY 1
 )
 SELECT
    day AS "Day"
-   , jobs_count AS "Jobs count"
-   , runs_count AS "Runs count"
    , bar(CAST(max_concurrent_jobs_count AS double) / max(max_concurrent_jobs_count) OVER (), 40, rgb(0, 155, 0), rgb(255, 0, 0)) AS "Max concurrent jobs count chart"
    , max_concurrent_jobs_count AS "Max concurrent jobs count"
-   , max_concurrent_job AS "Max concurrent job"
+   , max_concurrent_runs_count AS "Max concurrent runs count"
 FROM main
 ORDER BY 1;
