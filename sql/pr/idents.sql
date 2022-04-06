de286abefbce9053f389a81e8a608e8c661fc0f3
Jan Was <jan.was@starburstdata.com>
2022-04-06 15:21:13 +0200

    Make sure first query for the git connector runs in single thread

--- sql/pr/idents.sql
+++ sql/pr/idents.sql
@@ -1,3 +1,9 @@
+
+-- this is not a report, just a setup
+-- make sure the connector fetches the repo in a single thread
+SELECT count(*)
+FROM git.default.commits;
+
 DROP TABLE IF EXISTS memory.default.nodes;
 CREATE TABLE memory.default.nodes AS
     SELECT DISTINCT author_email AS email, author_name AS name
@@ -14,29 +20,29 @@ CREATE TABLE memory.default.edges AS
 
 DROP TABLE IF EXISTS memory.default.idents;
 CREATE TABLE memory.default.idents AS
-with recursive
-    cte(name1, name2, visited) as (
-        select name1, name2, array[name1]
-        from memory.default.edges
-        where name1 = name2
-        union all
-        select c.name1, e.name2, c.visited || e.name2
-        from cte c
-        inner join memory.default.edges e on e.name1 = c.name2
-        where not contains(c.visited, e.name2)
+WITH RECURSIVE
+    walk (name1, name2, visited) AS (
+        SELECT name1, name2, ARRAY[name1]
+        FROM memory.default.edges
+        WHERE name1 = name2
+        UNION ALL
+        SELECT w.name1, e.name2, w.visited || e.name2
+        FROM walk w
+        INNER JOIN memory.default.edges e ON e.name1 = w.name2
+        WHERE NOT contains(w.visited, e.name2)
     ),
-    res(name1, name2s) as (
-        select name1, array_agg(distinct name2 order by name2)
-        from cte
-        group by name1
+    result (name1, name2s) AS (
+        SELECT name1, array_agg(DISTINCT name2 ORDER BY name2)
+        FROM walk
+        GROUP BY name1
     ),
     grouped (names, emails) AS (
-        select
-            array_agg(distinct n.name order by n.name) as names,
-            array_agg(distinct n.email order by n.email) as emails
-        from res r
-        inner join memory.default.nodes n on n.name = r.name1
-        group by r.name2s
+        SELECT
+            array_agg(DISTINCT n.name ORDER BY n.name) AS names,
+            array_agg(DISTINCT n.email ORDER BY n.email) AS emails
+        FROM result r
+        INNER JOIN memory.default.nodes n ON n.name = r.name1
+        GROUP BY r.name2s
     )
 SELECT
     emails[1] AS email,
