0f97570e859643e58bb724cb8e17b1a4cfb51c6d
Michał Ślizak <michal.slizak+github@gmail.com>
2022-04-07 09:47:43 +0200

    Add statistics for top-authors

---
+++ sql/pr/top-authors.sql
@@ -0,0 +1,23 @@
+-- Top authors per year
+WITH mergers_per_year AS (
+    SELECT
+        year(c.commit_time AT TIME ZONE 'UTC') AS year,
+        ai.name || ' <' || ai.email || '>' AS identifier,
+        count(*) AS commits_count
+    FROM git.default.commits c
+    JOIN memory.default.idents ai ON ai.email = c.author_email OR CONTAINS(ai.extra_emails, c.author_email)
+    GROUP BY 1, 2),
+ranked_per_year AS (
+    SELECT
+        year,
+        identifier,
+        commits_count,
+        row_number() OVER (PARTITION BY year ORDER BY commits_count DESC) AS in_year_rank
+    FROM mergers_per_year)
+SELECT
+    year,
+    identifier,
+    commits_count
+FROM ranked_per_year
+WHERE in_year_rank < 6
+ORDER BY 1, 3 DESC
