760c19931010574b1edce0d56daf1468a4c9462b
Jan Was <jan.was@starburstdata.com>
2022-04-07 15:31:18 +0200

    Use inner joins in some reports

--- sql/pr/abandoned-prs.sql
+++ sql/pr/abandoned-prs.sql
@@ -6,7 +6,7 @@ SELECT
 , format('<a href="https://github.com/trinodb/trino/pull/%s">link</a>', pulls.number) AS link
 FROM
   unique_pulls pulls
-LEFT JOIN
+JOIN
   pull_commits ON pulls.number = pull_commits.pull_number
 WHERE pulls.owner = 'trinodb' AND pulls.repo = 'trino' AND pulls.state = 'open' AND NOT pulls.draft
 GROUP BY 1, 2
--- sql/pr/running-prs.sql
+++ sql/pr/running-prs.sql
@@ -6,7 +6,7 @@ SELECT
 , format('<a href="https://github.com/trinodb/trino/pull/%s">link</a>', pulls.number) AS link
 FROM
   unique_pulls pulls
-LEFT JOIN
+JOIN
   reviews ON pulls.number = reviews.pull_number
 WHERE pulls.owner = 'trinodb' AND pulls.repo = 'trino' AND pulls.state = 'open' AND NOT pulls.draft
 GROUP BY 1, 2, pulls.updated_at, 4
