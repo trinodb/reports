3c57e317d1a41713a34cfee9f9e0bd26b0478c48
Jan Was <jan.was@starburstdata.com>
2022-04-07 22:13:31 +0200

    Add current year winners to list of achievements

--- sql/git/achievements.sql
+++ sql/git/achievements.sql
@@ -19,17 +19,23 @@ WITH acha AS (
   SELECT * FROM memory.default.acquired_languages
   UNION ALL
   SELECT * FROM memory.default.acquired_words
+), masked AS (
+    SELECT
+        acq.*,
+        acq.author_name || ' <' || regexp_replace(acq.email, '(?<=.)[^@](?=[^@]*?@)|(?:(?<=@.)|(?!^)\G(?=[^@]*$)).(?=.*\.)', '*') || '>' AS fullname
+    FROM acq
 )
 SELECT
   acha.name,
   acha.description,
-  count(acq.id) AS num_winners,
-  FORMAT('%.2f', 100 * cast(count(acq.id) AS DOUBLE) / i.idents_count) AS percent_winners,
-  min(acq.achieved_at) AS first_achieved_at,
-  min_by(acq.achieved_in, acq.achieved_at) AS first_achieved_in,
-  slice(array_agg(acq.author_name || ' <' || regexp_replace(acq.email, '(?<=.)[^@](?=[^@]*?@)|(?:(?<=@.)|(?!^)\G(?=[^@]*$)).(?=.*\.)', '*') || '>' ORDER BY acq.num_achieved DESC, acq.achieved_at), 1, 3) AS top3
+  count(a.id) AS num_winners,
+  FORMAT('%.2f', 100 * cast(count(a.id) AS DOUBLE) / i.idents_count) AS percent_winners,
+  min(a.achieved_at) AS first_achieved_at,
+  min_by(a.achieved_in, a.achieved_at) AS first_achieved_in,
+  slice(array_agg(a.fullname ORDER BY a.num_achieved DESC, a.achieved_at) FILTER (WHERE year(a.achieved_at) = year(current_date)), 1, 3) AS recent_top3,
+  slice(array_agg(a.fullname ORDER BY a.num_achieved DESC, a.achieved_at), 1, 3) AS alltime_top3
 FROM acha
-LEFT JOIN acq ON acq.id = acha.id
+LEFT JOIN masked a ON a.id = acha.id
 CROSS JOIN (SELECT COUNT(*) AS idents_count FROM memory.default.idents) i
 GROUP BY acha.id, acha.name, acha.description, i.idents_count
 ORDER BY NULLIF(num_winners, 0) NULLS LAST, acha.name
