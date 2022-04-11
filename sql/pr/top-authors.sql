b866f528a27675b93e0b3158da3f630b22b57ad9
Jan Was <jan.was@starburstdata.com>
2022-04-11 14:43:40 +0200

    Add org to top mergers and authors

--- sql/pr/top-authors.sql
+++ sql/pr/top-authors.sql
@@ -1,24 +1,32 @@
 -- Top 10 authors per year
+-- note that org membership is current, so if someone joins an org, all their historical commits shift toward this org
 WITH mergers_per_year AS (
     SELECT
         year(c.commit_time AT TIME ZONE 'UTC') AS year,
-        ai.name || ' <' || regexp_replace(ai.email, '(?<=.)[^@](?=[^@]*?@)|(?:(?<=@.)|(?!^)\G(?=[^@]*$)).(?=.*\.)', '*') || '>' AS identifier,
+        ai.name,
+        regexp_replace(ai.email, '(?<=.)[^@](?=[^@]*?@)|(?:(?<=@.)|(?!^)\G(?=[^@]*$)).(?=.*\.)', '*') AS email,
+        m.org,
         count(*) AS commits_count
     FROM git.default.commits c
     JOIN memory.default.idents ai ON ai.email = c.author_email OR CONTAINS(ai.extra_emails, c.author_email)
-    GROUP BY 1, 2
+    LEFT JOIN members m ON CONTAINS(ai.logins, m.login)
+    GROUP BY 1, 2, 3, 4
 ),
 ranked_per_year AS (
     SELECT
         year,
-        identifier,
+        name,
+        email,
+        org,
         commits_count,
         row_number() OVER (PARTITION BY year ORDER BY commits_count DESC) AS in_year_rank
     FROM mergers_per_year
 )
 SELECT
     year,
-    identifier,
+    name,
+    email,
+    org,
     commits_count
 FROM ranked_per_year
 WHERE in_year_rank < 11
