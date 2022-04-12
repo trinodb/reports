4eef510d02870c5dc1e6c778796135c3ba08e92f
Jan Was <jan.was@starburstdata.com>
2022-04-12 13:46:52 +0200

    Fix charts in avg time to merge reports

--- sql/pr/avg-time-to-merge-per-org.sql
+++ sql/pr/avg-time-to-merge-per-org.sql
@@ -1,25 +1,28 @@
 -- Average time to merge a PR
 WITH
-per_month AS (
+per_month_org AS (
     SELECT
         date_trunc('month', date(p.merged_at)) AS month_merged
+      , coalesce(m.org, 'unaffiliated') AS org
       , sum(p.merged_at - p.created_at) AS total_time_to_merge
       , count(p.merged_at - p.created_at) AS num_merged
       , sum(p.merged_at - p.created_at) / count(p.merged_at - p.created_at) AS avg_time_to_merge
       , approx_percentile(round(to_milliseconds(p.merged_at - p.created_at) / (1000.0 * 3600.0 * 24.0), 2), ARRAY[0.5, 0.95, 0.99]) AS perc
     FROM unique_pulls p
+    LEFT JOIN members m ON m.login = p.user_login
     WHERE owner = 'trinodb' AND repo = 'trino'
-    GROUP BY 1
+    GROUP BY 1, 2
 )
 SELECT
     month_merged AS "Merged in month"
+  , org "Org"
   , num_merged "Merged PRs"
   , bar(to_milliseconds(avg_time_to_merge) / CAST(max(to_milliseconds(avg_time_to_merge)) OVER () AS double), 20, rgb(0, 155, 0), rgb(255, 0, 0)) AS "Avg TTM chart"
   , avg_time_to_merge AS "Avg TTM"
   , sum(total_time_to_merge) OVER w / sum(num_merged) OVER w AS "3 month avg TTM"
   , perc AS "Day percentiles 50, 95, 99"
-FROM per_month
+FROM per_month_org
 WHERE month_merged IS NOT NULL
 WINDOW w AS (ORDER BY month_merged ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)
-ORDER BY month_merged DESC
+ORDER BY org, month_merged DESC
 ;
--- sql/pr/avg-time-to-merge-since-creation.sql
+++ sql/pr/avg-time-to-merge-since-creation.sql
@@ -1,22 +1,24 @@
 -- Average time to merge per create month
-WITH monthly_data AS (
+WITH
+per_month AS (
     SELECT
-        date_trunc('month', date(created_at)) as month_created
-      , sum(merged_at - created_at) as total_time_to_merge
-      , count(merged_at - created_at) as num_merged
-      , approx_percentile(round(to_milliseconds(merged_at - created_at) / (1000.0 * 3600.0 * 24.0), 2), ARRAY[0.5, 0.95, 0.99]) AS perc
-    FROM unique_pulls
+        date_trunc('month', date(p.created_at)) AS month_created
+      , sum(p.merged_at - p.created_at) AS total_time_to_merge
+      , count(p.merged_at - p.created_at) AS num_merged
+      , sum(p.merged_at - p.created_at) / count(p.merged_at - p.created_at) AS avg_time_to_merge
+      , approx_percentile(round(to_milliseconds(p.merged_at - p.created_at) / (1000.0 * 3600.0 * 24.0), 2), ARRAY[0.5, 0.95, 0.99]) AS perc
+    FROM unique_pulls p
     WHERE owner = 'trinodb' AND repo = 'trino'
     GROUP BY 1
-    ORDER BY 1 DESC)
+)
 SELECT
     month_created AS "Created in month"
   , num_merged "Merged PRs"
-  , bar(to_milliseconds(total_time_to_merge) / CAST(max(to_milliseconds(total_time_to_merge)) OVER () AS double), 20, rgb(0, 155, 0), rgb(255, 0, 0)) AS "Avg TTM chart"
-  , total_time_to_merge / num_merged AS "Avg TTM"
+  , bar(to_milliseconds(avg_time_to_merge) / CAST(max(to_milliseconds(avg_time_to_merge)) OVER () AS double), 20, rgb(0, 155, 0), rgb(255, 0, 0)) AS "Avg TTM chart"
+  , avg_time_to_merge AS "Avg TTM"
   , sum(total_time_to_merge) OVER w / sum(num_merged) OVER w AS "3 month avg TTM"
   , perc AS "Day percentiles 50, 95, 99"
-FROM monthly_data
+FROM per_month
 WINDOW w AS (ORDER BY month_created ROWS BETWEEN 3 PRECEDING AND CURRENT ROW)
 ORDER BY month_created DESC
 ;
