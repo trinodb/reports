2182783198ecc4e78963fa7411b17cd3e36d98f5
Jan Was <jan.was@starburstdata.com>
2022-04-13 15:21:17 +0200

    Add PRs per author chart

---
+++ sql/pr/prs-per-author.sql
@@ -0,0 +1,30 @@
+-- PRs per author
+WITH
+prs_per_author AS (
+    SELECT p.user_login, count(*) AS num_prs
+    FROM unique_pulls p
+    GROUP BY 1
+)
+, histogram AS (
+    SELECT
+        num_prs AS key
+        , width_bucket(num_prs, ARRAY[1, 2, 3, 4, 5, 10, 100, 1000]) AS bucket
+        , count(*) AS value
+    FROM prs_per_author
+    GROUP BY 1
+)
+, grouped AS (
+    SELECT
+        bucket
+        , element_at(ARRAY['1', '2', '3', '4', '5 to 10', '10 to 100', '100 to 1000', 'over 1000'], bucket) AS range
+        , sum(value) AS value
+    FROM histogram
+    GROUP BY 1, 2
+)
+SELECT
+  range AS "Number of PRs pet author"
+  , value AS "Number of authors"
+  , bar(value / CAST(max(value) OVER () AS double), 20, rgb(0, 155, 0), rgb(255, 0, 0)) AS "Chart"
+FROM grouped
+ORDER BY bucket
+;
