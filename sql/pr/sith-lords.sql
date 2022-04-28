d33260694d6c0ced5f372d6178c44428251528ac
Michał Ślizak <michal.slizak+github@gmail.com>
2022-04-28 12:02:03 +0200

    Detect Sith lord pairs
    Detects pairs of developers which merge the most PRs
    PRs are sequences of commits with the same commit_time

---
+++ sql/pr/sith-lords.sql
@@ -0,0 +1,29 @@
+-- Always two, there are. No more. No less. A master and an apprentice
+WITH pairs AS (
+    SELECT
+        c.commit_time AT TIME ZONE 'UTC' AS commit_time,
+        ai.name AS ai_name,
+        ma.org AS ai_org,
+        ci.name AS ci_name,
+        mc.org AS ci_org,
+        count(*) AS commits_in_pr
+    FROM git.default.commits c
+        JOIN memory.default.gh_idents ai ON ai.email = c.author_email OR CONTAINS(ai.extra_emails, c.author_email)
+        JOIN memory.default.gh_idents ci ON ci.email = c.committer_email OR CONTAINS(ci.extra_emails, c.committer_email)
+        LEFT JOIN members ma ON CONTAINS(ai.logins, ma.login)
+        LEFT JOIN members mc ON CONTAINS(ci.logins, mc.login)
+    WHERE ai.email != ci.email
+    GROUP BY 1, 2, 3, 4, 5
+)
+SELECT
+    ai_name AS author_name,
+    ai_org AS author_org,
+    ci_name AS commiter_name,
+    ci_org AS commiter_org,
+    sum(commits_in_pr) AS commits,
+    count(*) AS pull_requests
+FROM pairs
+GROUP BY 1, 2, 3, 4
+ORDER BY pull_requests DESC
+LIMIT 50
+;
