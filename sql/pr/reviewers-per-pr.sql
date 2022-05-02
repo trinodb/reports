fd1babd91e32a02907a4c2ebe748833290ceb741
Jan Was <jan.was@starburstdata.com>
2022-05-02 14:30:55 +0200

    Add reviewers per pr histogram

---
+++ sql/pr/reviewers-per-pr.sql
@@ -0,0 +1,32 @@
+-- Reviewers per PR
+WITH
+reviews_per_pr AS (
+    SELECT r.pull_number, count(distinct r.user_login) AS num
+    FROM reviews r
+    JOIN unique_pulls p ON p.number = r.pull_number
+    WHERE r.user_login != p.user_login
+    GROUP BY 1
+)
+, histogram AS (
+    SELECT
+        num AS key
+        , width_bucket(num, ARRAY[1, 2, 3, 4, 5, 10, 20]) AS bucket
+        , count(*) AS value
+    FROM reviews_per_pr
+    GROUP BY 1
+)
+, grouped AS (
+    SELECT
+        bucket
+        , element_at(ARRAY['1', '2', '3', '4', '5 to 10', '10 to 20', 'over 20'], bucket) AS range
+        , sum(value) AS value
+    FROM histogram
+    GROUP BY 1, 2
+)
+SELECT
+  range AS "Reviewers per PR"
+  , value AS "Number of reviews"
+  , bar(value / CAST(max(value) OVER () AS double), 20, rgb(0, 155, 0), rgb(255, 0, 0)) AS "Chart"
+FROM grouped
+ORDER BY bucket
+;
