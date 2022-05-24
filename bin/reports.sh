47870f1912c803ad54e257b7e3fe93a14e129f82
Jan Was <jan.was@starburstdata.com>
2022-05-24 11:02:24 +0200

    Rename hive catalog

--- bin/reports.sh
+++ bin/reports.sh
@@ -37,7 +37,7 @@ function run_query() {
     echo >&2 "Executing query from $file"
     docker exec \
         $container_name \
-        trino --catalog hive --schema v2 \
+        trino --catalog trinocicd --schema v2 \
         -f "/tmp/$(basename "$file")" \
         --output-format=ALIGNED | ansi2html --inline | sed 's,| \(.*\) @https://\([^ ]*\),| <a href="https://\2">\1</a>,g'
 }
