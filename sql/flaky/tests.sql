dd4b142bb83ec3671ba3c628efdf719f07ec45fd
Jan Was <jan.was@starburstdata.com>
2022-07-25 13:01:52 +0200

    Only show flaky test annotations from last month, with min max dates

--- sql/flaky/tests.sql
+++ sql/flaky/tests.sql
@@ -10,18 +10,22 @@ suites AS (
 )
 -- jobs within workflow
 , runs AS (
-    SELECT cr.id, cr.repo, s.head_branch, cr.name
-    FROM check_runs cr JOIN suites s ON cr.check_suite_id = s.id
+    SELECT cr.id, cr.repo, s.head_branch, cr.name, cr.completed_at
+    FROM check_runs cr
+    JOIN suites s ON cr.check_suite_id = s.id
 )
 SELECT
-    cra.title AS test_name
+    a.title AS test_name
   , count(*) AS count
   , CAST(count(*) * 100.0 / (SELECT count(*) FROM suites) AS decimal(4,1)) AS "%"
-  , array_agg(DISTINCT substring(cra.message, 1, least(5000, coalesce(nullif(strpos(cra.message, U&'\000a'), 0), length(cra.message))))) messages
-FROM check_run_annotations cra
-WHERE cra.annotation_level = 'failure'
+  , min(r.completed_at) AS first_seen_at
+  , max(r.completed_at) AS last_seen_at
+  , array_agg(DISTINCT substring(a.message, 1, least(5000, coalesce(nullif(strpos(a.message, U&'\000a'), 0), length(a.message))))) messages
+FROM check_run_annotations a
+JOIN runs r ON r.id = a.check_run_id
+WHERE a.annotation_level = 'failure'
   -- for tests failures uploded as GitHub annotations, the "title" is "TestClass > testMethodName (additional info)?"
-  AND cra.title LIKE 'Test%'
-GROUP BY cra.title
+  AND a.title LIKE 'Test%'
+GROUP BY a.title
 ORDER BY count DESC
 ;
