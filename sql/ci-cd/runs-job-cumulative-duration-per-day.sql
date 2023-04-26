3a46bd5bb694fda13068ef1478960c536d7d7685
Jan Waś <jan.was@starburstdata.com>
2023-04-26 10:25:10 +0200

    Include all runs in cumulative jobs duration report

--- sql/ci-cd/runs-job-cumulative-duration-per-day.sql
+++ sql/ci-cd/runs-job-cumulative-duration-per-day.sql
@@ -1,16 +1,22 @@
 -- Runs cumulative job duration on master per day
+-- The cumulative duration should represent how much work is being done
+-- and given there's an upper limit on the number of runners available,
+-- correlate with the queue duration.
+-- Showing all runs from master branch in last 14 days, even failed ones,
+-- since there's a very low chance of all jobs finishing successfully.
 WITH runs AS (
   SELECT
-    r.id AS run_id
+      r.id AS run_id
+    , r.conclusion
     , cast(r.created_at AS date) AS day
     , count(j.id) AS jobs_count
     , sum(j.completed_at - j.started_at) AS duration
   FROM runs r
   JOIN jobs j ON r.id = j.run_id
   WHERE r.owner = 'trinodb' AND r.repo = 'trino' AND r.name = 'ci'
-  AND r.conclusion = 'success' AND r.created_at > CURRENT_DATE - INTERVAL '14' DAY
+  AND r.created_at > CURRENT_DATE - INTERVAL '14' DAY
   AND r.head_branch = 'master'
-  GROUP BY 1, 2
+  GROUP BY 1, 2, 3
 ),
 main AS (
   SELECT
@@ -20,6 +26,7 @@ main AS (
     , max(duration) AS max_dur
     , avg(duration) AS avg_dur
     , count(*) AS runs_count
+    , count(*) FILTER (WHERE r.conclusion = 'success') AS runs_ok_count
     , approx_percentile(to_milliseconds(duration), ARRAY[0.75, 0.90, 0.95, 0.99]) AS perc
   FROM runs r
   GROUP BY 1
@@ -28,6 +35,7 @@ SELECT
    day AS "Day"
    , bar(CAST(perc[3] AS double) / max(perc[3]) OVER (), 40, rgb(0, 155, 0), rgb(255, 0, 0)) AS "P₉₅ runs cumulative job duration chart"
    , runs_count AS "Runs count"
+   , runs_ok_count AS "Successful runs"
    , jobs_count AS "Max jobs count"
    , transform(perc, m -> parse_duration(cast(m as varchar) || 'ms')) AS "Runs job duration percentiles 75, 90, 95, 99"
    , min_dur AS "Min jobs dur"
