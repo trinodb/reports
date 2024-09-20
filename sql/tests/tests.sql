d6063d3e57d1f58fc18887722ade23d7b737e0c7
Jan Waś <jan.was@starburstdata.com>
2024-09-20 16:10:09 +0200

    Remove markdown escapes from tests report

--- sql/tests/tests.sql
+++ sql/tests/tests.sql
@@ -15,13 +15,12 @@ suites AS (
     JOIN suites s ON cr.check_suite_id = s.id
 )
 SELECT
-    -- escape markdown special characters with a single backslash
-    regexp_replace(a.title, '([._\\`*{}\[\]\(\)<>:|"''#$])', '\\$1') AS "Test name"
+    a.title AS "Test name"
   , count(*) AS "Count"
   , CAST(count(*) * 100.0 / (SELECT count(*) FROM suites) AS decimal(4,1)) AS "%"
   , min(r.completed_at) AS "First seen at"
   , max(r.completed_at) AS "Last seen at"
-  , array_agg(DISTINCT replace(regexp_replace(substring(a.message, 1, least(5000, coalesce(nullif(strpos(a.message, U&'\000a'), 0), length(a.message)))), '([._\\`*{}\[\]\(\)<>:|"''#$])', '\\$1'), chr(10), '<br/>')) "Messages"
+  , array_agg(DISTINCT replace(substring(a.message, 1, least(5000, coalesce(nullif(strpos(a.message, U&'\000a'), 0), length(a.message)))), chr(10), '<br/>')) "Messages"
 FROM check_run_annotations a
 JOIN runs r ON r.id = a.check_run_id
 WHERE a.annotation_level = 'failure'
